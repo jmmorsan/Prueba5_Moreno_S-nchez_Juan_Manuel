ad449faea7cc55e288931a01ff1cd3a2bc4a518a
Juanma <moreno.sanchez.juan.manuel@iescamas.es>
2024-12-18 14:35:02 +0100

    Corrección ejercicio1

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -2,35 +2,37 @@ package ejercicio1;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-	public static void main(String[]args) {
-		var scanner = new Scanner(System.in);
-		System.out.println("Escribe una frase");
-		String frase = scanner.nextLine();
-		
-		int largoFrase = frase.length();
-		int contadorEspacios = 0;
-		int contadorPalabras = 0;
-		int contadorFinal = 0;
-
-		for(int i = 0; largoFrase > i; i++) {
-			char a = frase.charAt(i);
-			contadorPalabras++;
-			
-			if(Character.isSpaceChar(a) ) {
-				contadorEspacios++;
-				for(int u = i; u < 0; u--) {
-					
-				}
-				
-			}
-			
-		}
-		
-		contadorFinal = contadorPalabras - contadorEspacios;
-		
-		System.out.println("Hay " + contadorFinal + " palabras en total");
-		
-		scanner.close();
-	}
-
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce una frase:");
+        String frase = sc.nextLine().trim();
+        
+        if (frase.isEmpty()) {
+            System.out.println("La frase está vacía.");
+            sc.close();
+            return;
+        }
+        
+        String[] palabras = frase.split("\\s+");
+        
+        int totalPalabras = palabras.length;
+        String palabraMayor = "", palabraMenor = palabras[0];
+        
+        for (String palabra : palabras) {
+            if (palabra.length() > palabraMayor.length()) {
+                palabraMayor = palabra;
+            }
+            if (palabra.length() < palabraMenor.length()) {
+                palabraMenor = palabra;
+            }
+            
+        }
+        
+        System.out.println("Número total de palabras: " + totalPalabras);
+        System.out.println("Palabra de mayor longitud: '" + palabraMayor + "' (" + palabraMayor.length() + " caracteres)");
+        System.out.println("Palabra de menor longitud: '" + palabraMenor + "' (" + palabraMenor.length() + " caracteres)");
+        
+        sc.close();
+    }
 }
